0a2d741237acb5a9d61440bb2b2c90745caef130
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2021-12-10 15:22:57 +0300

    2021-12-10-15-22

---
+++ kombi/devirdaim.h
@@ -0,0 +1,31 @@
+class Devirdaim{
+
+  private:
+    boolean calisma=false;
+    int suSicaklik=24;
+    int anlikSicaklik=24;
+    int butonPin=3;
+    int potPin=A0;
+    int calismaLed=8;
+  public:
+    Devirdaim(int bPin, int pPin, int cLed){
+      butonPin=bPin;
+      potPin=pPin;
+      calismaLed=cLed;
+    }
+    boolean checkCalisma(){
+      if(digitalRead(butonPin)==HIGH) return true; else return false;
+    }
+    void check(){
+      if(checkCalisma()==true) digitalWrite(calismaLed,HIGH); else digitalWrite(calismaLed,LOW);
+      Serial.print("Sistem Sıcaklığı :");
+      Serial.println(sicaklik());   
+    }
+    int sicaklik(){
+        int sDeger=analogRead(potPin);
+        sDeger=sDeger/10;
+        return sDeger;
+     
+    }
+  
+};
