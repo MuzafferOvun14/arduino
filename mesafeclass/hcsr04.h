a201d37a3a811e3c8a91367f6f89a5a70224c3e9
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2022-02-25 14:58:16 +0300

    fghfgh
    fghfg

--- mesafeclass/hcsr04.h
+++ mesafeclass/hcsr04.h
@@ -1,24 +1,36 @@
-
 class HCSR04{
+
   private:
-    int echoPin=2;    int trigPin=4;
+    int echoPin=0;
+    int trigPin=0;
   public:
     HCSR04(int ePin,int tPin){
-      echoPin=ePin;      trigPin=tPin;
+      echoPin=ePin;
+      trigPin=tPin;
     }
     void setup(){
-       pinMode(echoPin,INPUT);       pinMode(trigPin,OUTPUT); 
-     }
-     long readmm(){
-        digitalWrite(trigPin,LOW);        delayMicroseconds(3);        digitalWrite(trigPin,HIGH);
-        delayMicroseconds(10);        digitalWrite(trigPin,LOW);
-        long sure=pulseIn(echoPin,HIGH);        long mesafe=(sure/2)*0.343;
+      pinMode(echoPin,INPUT); 
+      pinMode(trigPin,OUTPUT); 
+    }
+    long read(){
+        digitalWrite(trigPin,LOW);
+        delayMicroseconds(3);
+        digitalWrite(trigPin,HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin,LOW);
+        long sure=pulseIn(echoPin,HIGH);
+        long mesafe=(sure/2)*0.343;
         return mesafe;
-     }
-     void writecm(){
-      long cm=readmm();
-      cm=cm/10;
-      Serial.print(cm);
-      Serial.println(" cm"); 
-     }
+    }
+    long readcm(){
+      long mesafe=read();
+      mesafe=mesafe/10;
+      return mesafe;
+    }
+    float readMeter(){
+      float mesafe=(float)readcm();
+      return mesafe/100;
+    }
+  
+  
 };
--- mesafeclass/mesafeclass.ino
+++ mesafeclass/mesafeclass.ino
@@ -1,12 +1,15 @@
 #include "hcsr04.h"
 HCSR04 mesafe(2,4);
+HCSR04 mesafe2(3,5);
 void setup() {
   mesafe.setup();
+  mesafe2.setup();
+  
   Serial.begin(9600);
 }
 
 void loop() {
- Serial.println(mesafe.readmm());
- mesafe.writecm();
+ Serial.println(mesafe.readMeter());
+  Serial.println(mesafe2.readMeter());
  delay(300);
 }
