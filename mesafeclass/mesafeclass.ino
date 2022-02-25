69a3b5f8feec75c6eecbc36079a792a4ed1a0b2a
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2022-02-25 15:17:54 +0300

    asdas
    adsasda

--- mesafeclass/mesafeclass.ino
+++ mesafeclass/mesafeclass.ino
@@ -1,15 +1,19 @@
 #include "hcsr04.h"
-HCSR04 mesafe(2,4);
-HCSR04 mesafe2(3,5);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3f,16,2); // Bu kodu kullanırken ekranda yazı çıkmaz ise 0x27 yerine 0x3f yazınız !!
+HCSR04 mesafe(3,5);
 void setup() {
+  lcd.begin();
   mesafe.setup();
-  mesafe2.setup();
-  
   Serial.begin(9600);
 }
-
 void loop() {
  Serial.println(mesafe.readMeter());
-  Serial.println(mesafe2.readMeter());
+ lcd.clear();
+  lcd.setCursor(0,0); // İlk satırın başlangıç noktası
+ lcd.print("Mesafe");
+ lcd.setCursor(0,1); // İkinci satırın başlangıç noktası
+ lcd.print((String)mesafe.readMeter()+" m");
  delay(300);
+ 
 }
